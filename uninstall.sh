2baf06ce4b040175e3c3f0e569b4d8795c46bb56
Leo Winter <git@lwinter.de>
2024-03-29 14:04:59 +0100

    Fix uninstall.sh script

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  )
 DAEMON_NAME=${SCRIPT_DIR##*/}
+RC_FILE_PATH=/data/rc.local
+STARTUP=$SCRIPT_DIR/install.sh
 
 rm /service/$DAEMON_NAME
 kill $(pgrep -f 'supervise $DAEMON_NAME')
 chmod a-x $SCRIPT_DIR/service/run
 $SCRIPT_DIR/restart.sh
+sed -i "\~$STARTUP~d" $RC_FILE_PATH
